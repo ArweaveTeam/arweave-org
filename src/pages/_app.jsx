de41f09623d0d687fa330dffcb9db6e296a674a8
Cedrik <cedrik.git@tryninja.io>
2023-01-19 09:39:59 -0600

    removed bg

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -7,7 +7,6 @@ import { useEffect, useRef } from 'react'
 import { Header } from '@/components/Header'
 
 import { Container } from '@/components/Container'
-import backgroundImage from '@/images/background.jpg'
 import { NavBar } from '@/components/NavBar'
 
 function usePrevious(value) {
@@ -37,15 +36,6 @@ export default function App({ Component, pageProps, router }) {
       <main>
         <div className="relative pt-10 pb-20 sm:py-24">
           <div className="bg-indigo-50 absolute inset-x-0 -top-48 -bottom-14 overflow-hidden">
-            <Image
-              className="absolute top-0 left-0 translate-y-[-10%] translate-x-[-55%] -scale-x-100 sm:left-1/2 sm:translate-y-[-6%] sm:translate-x-[-98%] lg:translate-x-[-106%] xl:translate-x-[-122%]"
-              src={backgroundImage}
-              alt=""
-              width={918}
-              height={1495}
-              priority
-              unoptimized
-            />
             <div className="absolute inset-x-0 top-0 h-40 bg-gradient-to-b from-white" />
             <div className="absolute inset-x-0 bottom-0 h-40 bg-gradient-to-t from-white" />
           </div>
