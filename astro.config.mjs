b0f26f063307d0cc75e2bad423921071d39ea737
Dylan Shade <63427984+dpshade@users.noreply.github.com>
2025-07-28 13:19:33 -0400

    chore: update astro.config.mjs for Vite configuration and add terser for build optimization; modify NavBar currentPathname to include .html extension in multiple pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,42 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  base: '.',
+  vite: {
+    logLevel: 'info',
+    define: {
+      __DATE__: `'${new Date().toISOString()}'`,
+    },
+    server: {
+      fs: {
+        // Allow serving files from hoisted root node_modules
+        allow: ['../..'],
+      },
+    },
+    build: {
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true
+        }
+      },
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            'react-vendor': ['react', 'react-dom'],
+          }
+        }
+      }
+    }
+  },
+  integrations: [
+    tailwind(),
+    react()
+  ],
   trailingSlash: 'always',
+  output: 'static'
 });
